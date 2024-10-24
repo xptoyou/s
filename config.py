8380a190144bf56ba8a1ef49d239f006f81b6a4f
xptoyou <xp.to.you.eg@gmail.com>
2024-10-25 00:01:09 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+MAIL_SERVER = 'smtp.your-email-provider.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'your-email@example.com'
+MAIL_PASSWORD = 'your-email-password'
+MAIL_DEFAULT_SENDER = 'your-email@example.com'
