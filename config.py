8d70efa1070ad0951969a29c4504e4929f7186d5
xptoyou <xp.to.you.eg@gmail.com>
2024-10-25 00:11:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-MAIL_SERVER = 'smtp.your-email-provider.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'your-email@example.com'
-MAIL_PASSWORD = 'your-email-password'
-MAIL_DEFAULT_SENDER = 'your-email@example.com'
+MAIL_SERVER = 'smtp.gmail.com'  # SMTP server for Gmail
+MAIL_PORT = 587  # Port for TLS
+MAIL_USE_TLS = True  # Use TLS
+MAIL_USERNAME = 'your-email@gmail.com'  # Your email address
+MAIL_PASSWORD = 'your-email-password'  # Your email account password
+MAIL_DEFAULT_SENDER = 'your-email@gmail.com'  # Default sender address
+
