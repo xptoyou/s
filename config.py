c1c759188410ff3722baf492ed040b19673c2f56
xptoyou <xp.to.you.eg@gmail.com>
2024-10-25 13:31:28 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-MAIL_SERVER = 'smtp.gmail.com'  # SMTP server for Gmail
-MAIL_PORT = 587  # Port for TLS
-MAIL_USE_TLS = True  # Use TLS
-MAIL_USERNAME = 'your-email@gmail.com'  # Your email address
-MAIL_PASSWORD = 'your-email-password'  # Your email account password
-MAIL_DEFAULT_SENDER = 'your-email@gmail.com'  # Default sender address
-
